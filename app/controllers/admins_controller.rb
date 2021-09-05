e4dfd69a4c57a1f2abaf88c91c79bf77ec5dbc38
yozakura <matchnokaori@gmail.com>
2021-09-06 00:25:41 +0900

    ブランチ切り替え

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,10 +6,24 @@ class AdminsController < ApplicationController
     @admin = Admin.new
   end
 
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to @admin
+    else
+      render 'new'
+    end
+  end
+
   def show
   end
 
   def edit
   end
 
+  private
+
+    def admin_params
+      params.require(:admin).permit(:company_id, :place, :place_remarks, :salary, :koutsuhi)
+    end
 end
