7401fa84b7e9512e6756ffd0239aedff187408d2
yozakura <matchnokaori@gmail.com>
2021-09-06 00:01:00 +0900

    collection_selectからselect2で検索選択実装

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,28 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
   end
 
   def show
+    @company = Company.find(params[:id])
   end
 
   def new
+    @company = Company.new
   end
 
   def edit
+    @company = Company.find(params[:id])
+  end
+
+  def destroy
+    Company.find(params[:id]).destroy
+    flash[:success] = "削除しました"
+    redirect_to companies_path
+  end
+
+  def import
+    Company.import(params[:file])
+    redirect_to companies_path
   end
 end
--- app/javascript/js/select.js
+++ app/javascript/js/select.js
@@ -1,3 +1,7 @@
+console.log("hello!!")
+
 $(function () {
   $('#animal').select2();
+
+  $('#admin_company_id').select2();
 });
