22566fa6f0750b5b829e8e7cf8edc40153f4ddba
yozakura <matchnokaori@gmail.com>
2021-09-14 00:41:20 +0900

    admin画面実装

--- app/javascript/js/select.js
+++ app/javascript/js/select.js
@@ -1,8 +1,21 @@
 console.log("hello!!")
 
 $(function () {
-  $('#admin_company_id').select2();
-  $('#admin_staff_id').select2();
+  $('#admin_company_id').select2({
+    width: 'resolve'
+  });
+  $('#admin_staff_id').select2({
+    width: 'resolve'
+  });
+
+  $(window).on('resize', function() {
+    $('admin_area').each(function() {
+        var formGroup = $(this),
+            formgroupWidth = formGroup.outerWidth();
+        formGroup.find('.select2-container').css('width', formgroupWidth);
+    });
+  });
+
   $('#admin_staff_id').val(null).trigger("change");
 
   $('#admin_table').DataTable();
