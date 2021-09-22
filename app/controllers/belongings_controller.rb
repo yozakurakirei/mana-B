9ef22cffeff460be21b1df596c0e2764653c790a
yozakura <matchnokaori@gmail.com>
2021-09-23 02:40:16 +0900

    申請ボタン修正

--- app/controllers/belongings_controller.rb
+++ app/controllers/belongings_controller.rb
@@ -5,7 +5,7 @@ class BelongingsController < ApplicationController
   end
 
   def create
-    @belonging = Belonging.create(admin_id: belonging_params[:admin_id],
+    @belongings = Belonging.create(admin_id: belonging_params[:admin_id],
                  user_id: belonging_params[:user_id])
     Apply.find(belonging_params[:apply_id]).destroy!
     redirect_to admin_applies_url(@belonging.admin), notice: "受注ID#{@belonging.admin_id}を承認しました"
